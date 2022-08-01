06c31f2a5e7c359eb6b2e24ca125d56f82151990
Rajesh_Daya_Omnipro <rajesh.daya@omni.pro>
2022-08-01 12:32:04 -0500

    create PageBuilderBlockTheme module folder

---
+++ PageBuilderBlockTheme/registration.php
@@ -0,0 +1,6 @@
+<?php
+Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'repoback_PageBuilderBlockTheme',
+    __DIR__
+);
